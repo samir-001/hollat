9a609e3f0cd00227ebc2a17fef50e12c24a2ff4c
samir-001 <samir.harg@yahoo.com>
2023-08-07 22:27:44 +0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductFormComponent } from './pages/product-form/product-form.component';
+import { ProductComponent } from './pages/product/product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",redirectTo:'home',pathMatch:"full"}
+  ,{path:"home",component:HomeComponent}
+  ,{path:"product/add",component:ProductFormComponent}
+  ,{path:"product/:id",component:ProductComponent}
+  ,{path:"**",redirectTo:'home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,48 @@
-import { NgModule } from '@angular/core';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule }from "@angular/common/http"
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { JumbotronComponent } from './components/jumbotron/jumbotron.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductHomeComponent } from './components/product-home/product-home.component';
+import { FormsModule } from '@angular/forms';
+
+// import function to register Swiper custom elements
+import { ProductFormComponent } from './pages/product-form/product-form.component';
+import { InputGroupComponent } from './components/input-group/input-group.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FooterComponent } from './components/footer/footer.component';
+import { ProductComponent } from './pages/product/product.component';
+
+// register Swiper custom elements
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavBarComponent,
+    JumbotronComponent,
+    HomeComponent,
+    ProductHomeComponent,
+    ProductFormComponent,
+    InputGroupComponent,
+    FooterComponent,
+    ProductComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas:[CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
