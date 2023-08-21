107797e986e5e6ec761bdac3e0839efdb0faad26
samir-001 <samir.harg@yahoo.com>
2023-08-21 17:47:35 +0300

    ui copying ..

--- src/app/components/contact-info/contact-info.component.ts
+++ src/app/components/contact-info/contact-info.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LanguageService } from 'src/app/services/language.service';
 
 @Component({
   selector: 'app-contact-info',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact-info.component.scss']
 })
 export class ContactInfoComponent {
+  constructor(public dictionary:LanguageService){
 
+  }
 }
